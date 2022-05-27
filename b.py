017c3312471bd87881a5b871a076e85273fd48f1
Smritikhadgi <smrtikhadgi98@gmail.com>
2022-05-27 13:25:39 +0545

    new commit

---
+++ b.py
@@ -0,0 +1,14 @@
+a=int(input("Enter number of students in class1"))
+b=int(input("Enter number of students in class2"))
+c=int(input("Enter number of students in class3"))
+d=a+b+c
+e=d/2
+x=e(int)
+if e==x:
+    print(e)
+else:
+    e=e+0.5
+    print(e)
+
+
+
